d94b534be1d84d586c2431ede05302dfcc97368f
John Shaffer <jdsha@pm.me>
2025-11-05 13:25:27 -0600

    Align license indentation

--- src/drop-in/object-cache.php
+++ src/drop-in/object-cache.php
@@ -9,8 +9,8 @@
  * Text Domain:       snapcache
  * Requires at least: 6.4
  * Requires PHP:      8.1
- * License: GPLv2 or later
- * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * License:           GPLv2 or later
+ * License URI:       https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
 // phpcs:disable Squiz.PHP.DiscouragedFunctions
