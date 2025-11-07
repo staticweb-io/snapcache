2a0bff26e6c99eeb55e2f6d825de28cca059b03b
John Shaffer <jdsha@pm.me>
2025-11-07 08:49:14 -0600

    Factor out getSnapCacheMemcached
    We will want access to \SnapCacheMemcached, not
    just \Memcached, because the former has logic like
    key construction.

--- src/Memcached.php
+++ src/Memcached.php
@@ -15,16 +15,37 @@ class Memcached {
     public static function getMemcached(
         $required = false,
     ): ?\Memcached {
+        $smc = self::getSnapCacheMemcached( $required );
+
+        if ( $smc instanceof \SnapCacheMemcached ) {
+            return $smc->mc;
+        }
+
+        return null;
+    }
+
+    /*
+     * Returns the \SnapCacheMemcached instance used by the
+     * object cache, or a new \SnapCacheMemcached instance if
+     * the object cache is not loaded or is not managed
+     * by this plugin.
+     *
+     * @param bool $required If true, throw an exception
+     * if unable to find a \SnapCacheMemcached instance.
+     */
+    public static function getSnapCacheMemcached(
+        $required = false,
+    ): ?\SnapCacheMemcached {
         global $wp_object_cache;
 
         if ( class_exists( 'SnapCacheMemcached' )
         && $wp_object_cache instanceof \SnapCacheMemcached ) {
-            return $wp_object_cache->mc;
+            return $wp_object_cache;
         }
 
         require_once SNAPCACHE_PATH . 'src/drop-in/object-cache.php';
         if ( class_exists( 'SnapCacheMemcached' ) ) {
-            return \SnapCacheMemcached::initAndBuild()->mc;
+            return \SnapCacheMemcached::initAndBuild();
         }
 
         if ( $required ) {
