61eb837444319ef41e59e75c18d5ef831a15d901
John Shaffer <jdsha@pm.me>
2025-11-10 09:32:21 -0600

    Add test that values persist between requests
    Make sure that we are actually hitting memcached
    and not using internal cache or WP's built-in
    object cache.

--- tests/integration/MemcachedObjectCacheTest.php
+++ tests/integration/MemcachedObjectCacheTest.php
@@ -12,4 +12,29 @@ final class MemcachedObjectCacheTest extends TestCase {
     {
         $this->evalEmbeddedTest( 'test-get-set-add-replace.php' );
     }
+
+    /**
+     * Test that cache values persist between requests
+     */
+    public function testPersistence(): void {
+        $k = uniqid();
+        $v = uniqid();
+
+        $this->wpCli( [ 'cache', 'set', $k, $v ] );
+        $lines = $this->wpCli( [ 'cache', 'get', $k ] );
+        $this->assertContains( $v, $lines );
+
+        $this->wpCli( [ 'cache', 'set', $k, $v, '', 600 ] );
+        $lines = $this->wpCli( [ 'cache', 'get', $k, '' ] );
+        $this->assertContains( $v, $lines );
+
+        $this->wpCli( [ 'cache', 'set', $k, $v, 'default', 600 ] );
+        $lines = $this->wpCli( [ 'cache', 'get', $k, 'default' ] );
+        $this->assertContains( $v, $lines );
+
+        $group = uniqid();
+        $this->wpCli( [ 'cache', 'set', $k, $v, $group, 600 ] );
+        $lines = $this->wpCli( [ 'cache', 'get', $k, $group ] );
+        $this->assertContains( $v, $lines );
+    }
 }
