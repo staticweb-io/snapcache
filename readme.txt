3d235165bf0a4fe94c34990243e634c539d4ebfb
John Shaffer <jdsha@pm.me>
2025-11-21 14:22:34 -0600

    Version 0.2.0

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: staticwebio
 Tags: performance, speed, memcached, object cache
 Requires at least: 6.4
 Tested up to: 6.8
-Stable tag: 0.1.0
+Stable tag: 0.2.0
 Requires PHP: 8.1
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
--- src/drop-in/object-cache.php
+++ src/drop-in/object-cache.php
@@ -3,7 +3,7 @@
  * Plugin Name:       SnapCache Object Cache
  * Plugin URI:        https://github.com/staticweb-io/snapcache
  * Description:       Object caching for WordPress.
- * Version:           0.1.0
+ * Version:           0.2.0
  * Author:            StaticWeb.io
  * Author URI:        https://github.com/staticweb-io/snapcache
  * Text Domain:       snapcache
